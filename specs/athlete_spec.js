30830d61e39ef68b00fa7404a89d0273e1f4cc27
Federico Zucca <zucca.federico@gmail.com>
2017-01-17 14:27:59 +0000

    updated

--- specs/athlete_spec.js
+++ specs/athlete_spec.js
@@ -5,14 +5,17 @@ describe("Athlete",function(){
   it("should start hydration at 100", function(){
     assert.equal(100,athlete.hydration);
   })
+  
   it("should start distance at 0", function(){
     assert.equal(0,athlete.distance);
   })
+  
   it("should run and increase distance and decrease hydration", function(){
     athlete.run(100);
     assert.equal(100,athlete.distance);
     assert.equal(99,athlete.hydration);
   });
+  
   it("should drink increase hydration by 10", function(){
     athlete.run(1000);
     athlete.drink();
